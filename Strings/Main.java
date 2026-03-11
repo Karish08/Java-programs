6000ca02ca94c9a01692e3791d6e0bfd74e8b30a
Karish08 <karishnithish@gmail.com>
2026-03-11 21:18:41 +0530

    first commit

---
+++ Strings/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first string: ");
+        String s = sc.nextLine();
+        System.out.print("Enter second string: ");
+        String t = sc.nextLine();
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        for (char c : t.toCharArray()) {
+            freq[c - 'a']--;
+        }
+        boolean isAnagram = true;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0) {
+                isAnagram = false;
+                break;
+            }
+        }
+        if (isAnagram) {
+            System.out.println("The strings are anagrams.");
+        } else {
+            System.out.println("The strings are NOT anagrams.");
+        }
+        sc.close();
+    }
+}
