ee05ee50bb286f659c4e6dc6ba26cd285d4ed43f
Karish08 <karishnithish@gmail.com>
2026-03-11 21:25:44 +0530

    first commit

--- Strings/Anagram.java
+++ Strings/Anagram.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Anagram {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter first string: ");
---
+++ Strings/ReplaceElements.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ReplaceElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements of array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            int max = -1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] > max) {
+                    max = arr[j];
+                }
+            }
+            arr[i] = max;
+        }
+        System.out.println("Modified array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        sc.close();
+    }
+}
