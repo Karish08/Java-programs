e6c655481ab7ff15138404b7f9c6816aee36a568
Karish08 <karishnithish@gmail.com>
2026-03-13 11:27:23 +0530

    added

---
+++ simple programs/inputoutput.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class inputoutput {
+    public static void main(String args[] ) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        sc.nextLine();
+        String sr=sc.nextLine();
+        System.out.println(n*2);
+        System.out.println(sr);
+    }
+}
