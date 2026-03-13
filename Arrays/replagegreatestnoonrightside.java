956edd8274286ec1f9d42d0bd8c24e31bd3a8c16
Karish08 <karishnithish@gmail.com>
2026-03-13 11:10:56 +0530

    added

---
+++ Arrays/replagegreatestnoonrightside.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class replagegreatestnoonrightside {
+    public static int[] replaceElements(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int max = -1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] > max) {
+                    max = arr[j];
+                }
+            }
+            arr[i] = max;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] result = replaceElements(arr);
+
+        System.out.println("Modified array:");
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
